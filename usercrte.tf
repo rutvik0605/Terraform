ae8b4ea93ff424ede507cf93a7faf226c9c885b7
Rutvik Honale <159903554+rutvik0605@users.noreply.github.com>
2024-04-10 17:01:36 +0530

    Create usercrte.tf

---
+++ usercrte.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_user" "lb" {
+  name = "TerraUser"
+  path = "/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "lb" {
+  user = aws_iam_user.lb.name
+}
